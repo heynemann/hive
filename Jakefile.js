12d8e5be8c6cd03658dbf54d92d7bb102c82b564
Bernardo Heynemann <heynemann@gmail.com>
2010-11-12 18:16:55 -0200

    Testing in jakefile

--- Jakefile.js
+++ Jakefile.js
@@ -1,7 +1,26 @@
-var sys = require('sys');
+#!/usr/bin/env node
 
-desc('This is the default task.');
-task('default', [], function (params) {
-    console.log('This is the default task.');
+var sys = require('sys');
+var exec = require('child_process').exec;
+function puts(error, stdout, stderr) { sys.puts(stdout) }
+
+var execute = function(command, callback) {
+    if (callback == null) {
+        callback = puts;
+    }
+    exec(command, callback);
+};
+
+desc('Default task - runs tests');
+task('default', ['test:run'], function (params) {
+});
+
+namespace('test', function() {
+
+    desc('Runs unit tests');
+    task('run', [], function (params) {
+        execute('cd src/test && expresso')
+    });
+
 });
 
