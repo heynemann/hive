960ffd9bba0c469229dc1797410cadc8b7481300
Fábio Miranda Costa <fabio.costa@corp.globo.com>
2010-11-19 18:13:07 -0200

    splitting the settings to a new file and defined to use haml as the templating language

--- src/app.js
+++ src/app.js
@@ -1,54 +1,30 @@
-
-/**
- * Module dependencies.
- */
-
-var express = require('express');
-var app = module.exports = express.createServer();
+require.paths.push(__dirname);
 var http = require('http');
-
-// Configuration
-
-app.configure(function(){
-    app.set('views', __dirname + '/views');
-    app.use(express.bodyDecoder());
-    app.use(express.methodOverride());
-    app.use(express.compiler({ src: __dirname + '/public', enable: ['less'] }));
-    app.use(app.router); 
-    app.use(express.staticProvider(__dirname + '/public'));
-});
-
-app.configure('development', function(){
-    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-
-app.configure('production', function(){
-    app.use(express.errorHandler());
-});
+var app = module.exports = require('settings').app;
 
 // Routes
 
 app.get('/', function(req, res){
-    res.render('index.jade', {
+    res.render('index.haml', {
         locals: {
             title: 'Express'
         }
     });
 });
 
-function getUrl(res, host, port, url) {
-    var request = http.createClient(port, host).request("GET", url, {});
-    request.addListener("response", function (response) {
+var getUrl = function(res, host, port, url) {
+    var request = http.createClient(port, host).request('GET', url, {});
+    request.addListener('response', function (response) {
         res.writeHead(response.statusCode, response.headers);
-        response.addListener("data", function (chunk) {
+        response.addListener('data', function (chunk) {
             res.write(chunk);
         });
-        response.addListener("end", function () {
+        response.addListener('end', function () {
             res.end();
         });
     });
     request.end();
-}
+};
 
 app.get(/^\/hive\/ping\/?(.*)$/, function(req, res) {
     var msg = 'pong';
@@ -58,14 +34,14 @@ app.get(/^\/hive\/ping\/?(.*)$/, function(req, res) {
     res.send(msg);
 });
 
-app.get(/^\/([^/]+)\/([^/]+)\/?(.*)$/, function(req, res){
+app.get(/^\/([^\/]+)\/([^\/]+)\/?(.*)$/, function(req, res){
     var user = req.params[0];
     var api = req.params[1];
     var rest = req.params[2];
 
     if (user == 'hive' && api == 'apiping') {
-        var host = "localhost"
-        var url = "/hive/ping/" + rest;
+        var host = 'localhost';
+        var url = '/hive/ping/' + rest;
         var port = 3000;
         getUrl(res, host, port, url);
         return;
---
+++ src/settings.js
@@ -0,0 +1,26 @@
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var app = module.exports.app = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+    app.set('view engine', 'haml');
+    app.set('views', __dirname + '/views');
+    app.use(express.bodyDecoder());
+    app.use(express.methodOverride());
+    app.use(express.compiler({ src: __dirname + '/public', enable: ['less'] }));
+    app.use(app.router);
+    app.use(express.staticProvider(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+    app.use(express.errorHandler());
+});
